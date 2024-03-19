0da4a26354288d6e9488897b506ab4117948c0a7
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-03-19 19:58:23 +0000

    yappa yap

--- components/screens/ViewProducts.js
+++ components/screens/ViewProducts.js
@@ -1,70 +1,70 @@
 import * as React from "react";
 import { Text, Button, View, StyleSheet, Pressable } from "react-native";
+import * as Notifications from "expo-notifications";
 
-import { fetchItems } from '../../sql';
+import { fetchItems } from "../../sql";
 
-Notifications.setNotificationHandler({
-    handleNotification: async () => {
-      return {
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-        shouldShowAlert: true,
-      };
-    },
-  });
-
-  
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => {
+//     return {
+//       shouldPlaySound: false,
+//       shouldSetBadge: false,
+//       shouldShowAlert: true,
+//     };
+//   },
+// });
 
 const ViewProduct = ({ navigation, route }) => {
-    const [products, setProducts] = React.useState([])
+  const [products, setProducts] = React.useState([]);
 
-
-
-    async function listAllDbRecords() {
+  async function listAllDbRecords() {
+    console.log("Get")
+    let result = await fetchItems();
+    setProducts(result);
+    console.log(JSON.stringify(JSON.stringify(products)));
+    console.log()
+  }
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.button} onPress={listAllDbRecords}>
+        <Text style={styles.buttonText}>Show Products</Text>
+      </Pressable>
+      <View>
+        {products.map((product) => (
+          <Text>
+            {product.id}: {product.title}
+          </Text>
+        ))}
         
-        let result = await fetchItems()
-        setProducts(result)
-        console.log(JSON.stringify(JSON.stringify(products)));
-    }
-    return (
-        <View style={styles.container}>
-            <Pressable style={styles.button} onPress={listAllDbRecords}>
-                <Text style={styles.buttonText}>Show Products</Text>
-            </Pressable>
-            <View>
-                {products.map((product) => (
-                    <Text>{product.id}: {product.title}</Text>
-                ))}
-            </View>
-        </View>
-    );
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: 30
-    },
-    text: {
-        marginTop: 50,
-        fontSize: 20,
-    },
-    button: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: '#6f2da8',
-        borderRadius: 5,
-        marginBottom: 20,
-        width: 180,
-        height: 40,
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 18,
-    },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  text: {
+    marginTop: 50,
+    fontSize: 20,
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#6f2da8",
+    borderRadius: 5,
+    marginBottom: 20,
+    width: 180,
+    height: 40,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
 });
 
 export default ViewProduct;
-
